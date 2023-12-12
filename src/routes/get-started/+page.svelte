b8f41bf0d3b114e05f45f2eb94424c22b1946a4a
Tyler Gleave <tyler@tylergleave.com>
2023-12-12 04:34:29 -0700

    Button positioned properly. Form started.

--- src/routes/get-started/+page.svelte
+++ src/routes/get-started/+page.svelte
@@ -3,6 +3,20 @@
 	<meta name="description" content="RememberMeObituaries.com offers professionally written, personal, and impactful obituaries and eulogies." />
 </svelte:head>
 
-<div class="mt-6">
-    <h1>Tell us about your loved one</h1>
+<div class="mt-6 container">
+	<h1 class="text-4xl mb-5">Tell us about your loved one</h1>
+	<label for="name" class="block text-sm font-medium leading-6 text-gray-900">Their name</label>
+	<div class="mt-2">
+		<input type="text" name="name" id="name" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6">
+	</div>
+
+	<label for="dob" class="mt-5 block text-sm font-medium leading-6 text-gray-900">Their birthdate</label>
+	<div class="mt-2">
+		<input type="text" name="dob" id="dob" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6">
+	</div>
+	
+	<label for="dob" class="mt-5 block text-sm font-medium leading-6 text-gray-900">Describe them in a few words</label>
+	<div class="mt-2">
+		<textarea name="about" id="about" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-500 sm:text-sm sm:leading-6"></textarea>
+	</div>
 </div>
